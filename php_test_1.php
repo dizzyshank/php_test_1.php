293a0c76fcde9c91c555fd9198e16e0c430731d5
homma reo <uvuw092544@gmail.com>
2022-12-18 10:10:52 +0000

    課題2

---
+++ php_test_1.php
@@ -0,0 +1,54 @@
+<?php
+
+$total = 0;
+echo $total;
+
+for ($i = 0; $i <= 100; $i++) {
+    $total += $i;
+}
+echo $total;
+
+$animals = array("dog", "cat", "panda");
+
+foreach($animals as $animal){
+    echo "要素は" . $animal;
+    echo "\n";
+}
+
+#課題1
+$name = "reo";
+
+$name = "reo";
+if ($name = "reo") {
+    echo "私は あなたの名前 です";
+} else {
+    echo "あなたの名前ではありません";
+}
+
+#課題2
+$total = 0;
+echo $total;
+
+for ($i =0; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
+
+#課題3
+$fruits = array("さくらんぼ", "メロン", "梨", "葡萄", "桃");
+
+foreach($fruits as $fruit){
+    echo "好きなフルーツは" . $fruit;
+    echo "\n";
+}
+
+#課題4
+
+for($i = 1; $i <= 100; $i++){
+    
+    //5で割り切れたら{}内を実行する
+    if($i % 5 == 0){
+        echo $i;
+        echo "\n";
+    }
+}
